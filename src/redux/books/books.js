b8b26191bbf72dead7bb264fe1e764745c20e116
mullenslucas <mullenlucas@gmail.com>
2022-09-26 18:12:53 -0300

    dev redux axns book list

---
+++ src/redux/books/books.js
@@ -0,0 +1,28 @@
+// Actions for books 
+const ADD_BOOK   = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK'
+
+const initialState = [];
+
+// Reducer section
+const reducerbookstore = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+// Action creators section
+export const addBook = (book) => ({
+  type: ADD_BOOK, book,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK, id,
+});
+
+export default reducerbookstore;
