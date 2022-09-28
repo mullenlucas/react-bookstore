b90ebdd6504f25d62657261d433c046cf6640e83
mullenslucas <mullenlucas@gmail.com>
2022-09-28 14:52:20 -0300

    update categories button to use dispatch

--- src/display/Categories.js
+++ src/display/Categories.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { statusChecker } from '../redux/categories/categories';
 
 function Categories() {
+  const dispatch = useDispatch();
   return (
     <div className="categ-sec">
-      <button type="button" id="check-status">Check status</button>
+      <button
+        type="button"
+        id="check-status"
+        onClick={() => dispatch(statusChecker())}
+      >
+        Check status
+      </button>
     </div>
   );
 }
