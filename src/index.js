17779de0c1affcd39a56c10648ebefc66fbea8dd
mullenslucas <mullenlucas@gmail.com>
2022-09-28 14:38:45 -0300

    add provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import BooksLibrary from './components/BooksLibrary';
 import Categories from './display/Categories';
+import bookstore from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
+        <Route
+          path="/"
+          element={(
+            <Provider store={bookstore}>
+              <App />
+            </Provider>
+        )}
+        >
           <Route path="/" element={<BooksLibrary />} />
           <Route path="/categories" element={<Categories />} />
         </Route>
