377aae31f42cc8c970d1b413b9e27fddf78e0592
mullenslucas <mullenlucas@gmail.com>
2022-09-28 14:42:43 -0300

    update books array to be taken from useSelector

--- src/components/BooksLibrary.js
+++ src/components/BooksLibrary.js
@@ -1,40 +1,13 @@
-import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Book from './BooksGen';
 import Form from './Form';
 
-const books = [
-  {
-    id: 1,
-    title: 'Tyrant',
-    genres: 'History',
-    progress: 94,
-    author: 'Valerio Massimo Manfredi',
-    chapter: 24,
-  },
-  {
-    id: 2,
-    title: 'Harry Potter and the Prisoner of Azkaban',
-    genres: 'Fantasy',
-    progress: 34,
-    author: 'J.K. Rowling',
-    chapter: 9,
-  },
-  {
-    id: 3,
-    title: 'The Dreaming City',
-    genres: 'Fantasy',
-    progress: 16,
-    author: 'Michael Moorcock',
-    chapter: 3,
-  },
-];
-
 function BooksLibrary() {
-  const [lib] = useState(books);
+  const books = useSelector((state) => state.books);
   return (
     <>
       <div id="books-list">
-        {lib.map((book) => (
+        {books.map((book) => (
           <Book key={book.id} book={book} />
         ))}
       </div>
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,7 +2,32 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'Tyrant',
+    genres: 'History',
+    progress: 94,
+    author: 'Valerio Massimo Manfredi',
+    chapter: 24,
+  },
+  {
+    id: 2,
+    title: 'Harry Potter and the Prisoner of Azkaban',
+    genres: 'Fantasy',
+    progress: 34,
+    author: 'J.K. Rowling',
+    chapter: 9,
+  },
+  {
+    id: 3,
+    title: 'The Dreaming City',
+    genres: 'Fantasy',
+    progress: 16,
+    author: 'Michael Moorcock',
+    chapter: 3,
+  },
+];
 
 // Reducer section
 const reducerBookstore = (state = initialState, action) => {
