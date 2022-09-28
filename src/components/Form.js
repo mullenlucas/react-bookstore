63fcc472fe2483798a862745e47fcd4dff7a4964
mullenslucas <mullenlucas@gmail.com>
2022-09-28 14:48:46 -0300

    update form to use dispatch and states

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function Form() {
+  const dispatch = useDispatch();
+  const [bookTitle, setbookTitle] = useState('');
+  const [bookAuthor, setbookAuthor] = useState('');
   return (
     <div id="add-div">
       <h2>Add a New Book!</h2>
       <form id="add-book-form">
-        <input id="title" placeholder="Title" type="text" />
-        <input id="author" placeholder="Author" type="text" />
-        <button type="button" className="add-btn">ADD BOOK</button>
+        <input id="book-title" value={bookTitle} onInput={(e) => setbookTitle(e.target.value)} placeholder="Book Title" type="text" />
+        <input id="book-author" value={bookAuthor} onInput={(e) => setbookAuthor(e.target.value)} placeholder="Book Author" type="text" />
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(addBook({
+              title: bookTitle,
+              author: bookAuthor,
+              id: uuidv4(),
+            }));
+            setbookAuthor('');
+            setbookTitle('');
+          }}
+        >
+          ADD BOOK
+        </button>
       </form>
     </div>
   );
